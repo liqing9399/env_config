8a904c0ec4eb95f35e04fe679e3339febcaf98f5
litao-xm <litao@streamcomoputing.com>
2023-08-21 17:42:04 +0800

    [Refactor](): Description: ... remove lt_test ...

--- lt_test/tpybind11/cm.sh
+++
@@ -1,9 +0,0 @@
-#######s############################################
-#filename      : cm.sh
-#author        : litao
-#e-mail        : Tao.Li@streamcomputing.com
-#create time   : 2023-05-24 10:49:09
-#last modified : 2023-05-24 10:49:09
-####################################################
-#!/bin/bash
-g++ -O0 -g -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) example.cpp -o emp$(python3-config --extension-suffix)
